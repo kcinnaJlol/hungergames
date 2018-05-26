efd4b98e1a79249d77f6feeff5417773a085f68d
Jannick <jannick.van.eeghem@gmail.com>
2018-05-26 13:08:34 +0200

    Gonna add a better license

--- src/main/java/com/gmail/jannick/van/eeghem/hungergames/HungerGames.java
+++ src/main/java/com/gmail/jannick/van/eeghem/hungergames/HungerGames.java
@@ -2,6 +2,9 @@ package com.gmail.jannick.van.eeghem.hungergames;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -13,6 +16,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class HungerGames extends JavaPlugin {
 	private final FileConfiguration config = getConfig();
 	private List<GameQueue> queues = new ArrayList<>();
+	private final Logger log = getLogger();
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
@@ -22,6 +26,16 @@ public class HungerGames extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
+		Map<String, Object> vals = config.getValues(true);
+		if(vals.containsKey("queues")) {
+			Object tqueues = vals.get("queues");
+			if (tqueues instanceof Map<?, ?>) {
+				@SuppressWarnings("unchecked")
+				Map<String, Object> queues = (Map<String, Object>) tqueues;
+			}
+		}else {
+			log.log(Level.CONFIG, "There are no queuers");
+		}
 		super.onEnable();
 	}
 
